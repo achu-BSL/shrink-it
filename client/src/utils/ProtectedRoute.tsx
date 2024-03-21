2525c78677b83bb5b2643e36ed2f55676d8375e3
achu-BSL <asharudeenalikt@gmail.com>
2024-03-21 10:34:52 +0530

    remove old client folder

--- client/src/utils/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useUserStore } from "../store/useUserStore"
-
-const ProtectedRoute = () => {
-    const user = useUserStore(state => state.user);
-    return user ? <Outlet/>: <Navigate to="/login"/>
-};
-
-export default ProtectedRoute;
