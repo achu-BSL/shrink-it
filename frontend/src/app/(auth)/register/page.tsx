4f3564aae51e0c4af30fcff9741c3dd1f6adcb62
achu-BSL <asharudeenalikt@gmail.com>
2024-04-15 11:38:24 +0530

    update register ui

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -13,23 +13,23 @@ const Register = () => {
         <div className="self-end h-10 w-[80%] bg-primary rounded-s-2xl"></div>
         <div>
           <div className="py-5">
-            <h1 className="text-center text-lg">Register</h1>
+            <h1 className="text-center text-lg font-lora">Register</h1>
           </div>
           <form onSubmit={submitHandler}>
             <div className="flex flex-col gap-2 px-3">
               <Input label="Username" type="text" />
               <Input label="Password" type="password" />
               <Input label="Confirm-Password" type="password" />
-              <button className="border-2 border-[#4ecdc4] py-1 hover:bg-primary rounded-md">
+              <button className="border-2 border-[#4ecdc4] py-1 hover:bg-primary rounded-md hover:text-white">
                 Register
               </button>
             </div>
           </form>
         </div>
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-1 flex items-center justify-center font-poppins">
           <p>
             Already have account?{" "}
-            <Link className="text-blue-600" href="/login">
+            <Link className="text-blue-600 font-lora" href="/login">
               Login
             </Link>
           </p>
